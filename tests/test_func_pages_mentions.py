47f7e3dc5d47079058b2eeb250e95c4f6897babe
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-11-08 21:13:54 +0100

    mentions + functional tests associated with

---
+++ tests/test_func_pages_mentions.py
@@ -0,0 +1,53 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('window-size=1920x1080')
+
+
+class TestHomePage(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = webdriver.Chrome(
+            executable_path='/mnt/c/webdrivers/chromedriver.exe',
+            options=chrome_options,
+        )
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.driver.quit()
+
+    def test_verify_elements_in_mentions(self):
+        self.driver.get(self.live_server_url)
+        time.sleep(5)
+        # user opens the page and sees header section
+        # verify that logo is loaded
+        logo = self.driver.find_element_by_id("logo")
+        source = logo.get_attribute("src")
+        self.assertEqual(
+            source,
+            self.live_server_url
+            + '/static/dist/assets/img/logo/logo.svg',
+        )
+        # verify page H1 title
+        title = self.driver.find_element_by_id("punch-line").text
+        self.assertEqual(title, 'MENTIONS')
+
+    def test_redirection_to_logout(self):
+        mentions_url = self.live_server_url + reverse("logout")
+        self.driver.get(self.live_server_url)
+        time.sleep(5)
+        self.driver.find_element_by_id("selections-logout").click()
+        self.assertEqual(self.driver.current_url, mentions_url)
+        time.sleep(5)
