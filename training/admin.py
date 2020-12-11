e50b0a985447dbbf0e9ebe06979b4564f3430ece
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-12-11 18:02:54 +0100

    quality check

--- training/admin.py
+++ training/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Practice, Category
+from .models import Category, Practice
 
 admin.site.register(Practice)
 admin.site.register(Category)
--- training/models.py
+++ training/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
-
 # Create your models here.
 
 
